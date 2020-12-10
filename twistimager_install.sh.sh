ec3ddceb5b351ee8cdd3824a8d31b5e57a7d57f6
mobilegmYT <44128563+mobilegmYT@users.noreply.github.com>
2020-12-09 17:49:43 -0700

    Update twistimager_install.sh.sh

--- twistimager_install.sh.sh
+++ twistimager_install.sh.sh
@@ -1,1 +1,5 @@
-sudo apt install rpi-imager
+#!/bin/bash
+
+wget https://github.com/mobilegmYT/TwisterOS-imager/raw/master/TwisterOS_imager_armhf.deb
+sudo apt install ./TwisterOS_imager_armhf.deb
+rm TwisterOS_imager_armhf.deb
